18788e6208f22ccda6a81957c8a6d92921b8335e
Zachary Lederman <zlederman00@gmail.com>
2025-01-24 16:33:34 -0500

    lazy bones

--- frontend/src/lib/client.ts
+++ frontend/src/lib/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const ENDPOINT = import.meta.env.VITE_ENDPOINT
+export const ENDPOINT = "https://lamport-clock-viz-production.up.railway.app"
 
 export const client = axios.create({
 	baseURL: ENDPOINT
