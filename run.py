a0a32039f12582ba86f6f7d0566df75ebb8e331c
Zachary Lederman <zlederman00@gmail.com>
2025-01-24 16:19:10 -0500

    added port

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import multiprocessing as mp
 import argparse
+import os
 
 from src.server import start_server
 import uvicorn
@@ -10,5 +11,6 @@ import uvicorn
 
 
 if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
     app = start_server()
     uvicorn.run(app, host="0.0.0.0", port=8000)
